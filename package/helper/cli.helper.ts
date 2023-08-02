d9134ed72e2cf45d2a15f7f5abbdf1097bf59a28
Raymond <akdfhr2@gmail.com>
2023-08-02 19:16:25 +0900

    chore: 패키지 분리

--- package/helper/cli.helper.ts
+++
@@ -1,8 +0,0 @@
-import { OptionValues } from "commander"
-type CLI_KEYS =  'number' | 'path'
-type CLI_OPTIONS = {
-    [key in  CLI_KEYS]? : string;
-} & { help :boolean ; target : 'programmers' | 'baekjoon' }
-export const parseArg = (argv : OptionValues) => {
-    return argv as CLI_OPTIONS
-}
