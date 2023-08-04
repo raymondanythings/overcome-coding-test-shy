f6a050cc710a8d1059957a9d736c874814899325
Raymond <akdfhr2@gmail.com>
2023-08-04 13:29:30 +0900

    DOCS: 이진탐색 알고리즘 이론 정리 - 계속
    - TODO: 파라메트릭 서치 개념 정리

--- 이진탐색(BinarySearch)/프로그래머스/lv4/징검다리/solution.js
+++ 이진탐색(BinarySearch)/프로그래머스/lv4/징검다리/solution.js
@@ -1,4 +1,31 @@
 function solution(distance, rocks, n) {
   var answer = 0;
+  rocks.sort((a, b) => a - b);
+  let left = 1;
+  let right = distance;
+
+  while (left <= right) {
+    let prev = 0;
+    let cnt = 0;
+    const mid = ~~((left + right) / 2);
+    rocks.forEach((rock) => {
+      if (rock - prev <= mid) {
+        cnt++;
+      } else {
+        prev = rock;
+      }
+    });
+
+    if (cnt > n) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+      console.log(left, answer);
+      answer = Math.max(left, answer);
+    }
+  }
+
   return answer;
 }
+
+console.log(solution(25, [2, 14, 11, 21, 17], 2));
